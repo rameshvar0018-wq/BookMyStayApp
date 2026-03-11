0bcf679a32fb07667fd3bd7f3fd49103736235e1
rameshvar0018-wq <rameshvar0018@gmail.com>
2026-03-11 19:53:12 +0530

    UC2

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,16 +1,64 @@
+abstract class Room {
+
+    protected int numberOfBeds;
+    protected int size;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int size, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.size = size;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomInfo(String roomType, int available) {
+        System.out.println(roomType + ":");
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+        System.out.println("Available: " + available);
+        System.out.println();
+    }
+}
+
+class SingleRoom extends Room {
+
+    public static int available = 5;
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public static int available = 3;
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public static int available = 2;
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
 public class BookMyStayApp {
 
+    public static void main(String[] args) {
 
-        /**
-         * Application entry point.
-         * This method is the first method executed
-         * when the program is launched by the JVM.
-         *
-         * @param args Command-line arguments
-         */
-        public static void main(String[] args) {
+        System.out.println("Hotel Room Initialization\n");
 
-            System.out.println("Welcome to the Hotel Booking Management System");
-            System.out.println("System initialized successfully.");
-        }
+        SingleRoom single = new SingleRoom();
+        DoubleRoom dbl = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        single.displayRoomInfo("Single Room", SingleRoom.available);
+        dbl.displayRoomInfo("Double Room", DoubleRoom.available);
+        suite.displayRoomInfo("Suite Room", SuiteRoom.available);
     }
+}
