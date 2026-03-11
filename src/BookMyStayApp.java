fa791979f5b892fe59da4fdbadd7e5db83bb6fe7
rameshvar0018-wq <rameshvar0018@gmail.com>
2026-03-11 19:38:27 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,16 @@
+public class BookMyStayApp {
+
+
+        /**
+         * Application entry point.
+         * This method is the first method executed
+         * when the program is launched by the JVM.
+         *
+         * @param args Command-line arguments
+         */
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to the Hotel Booking Management System");
+            System.out.println("System initialized successfully.");
+        }
+    }
